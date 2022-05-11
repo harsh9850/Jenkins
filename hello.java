6d9cc85a6c2e1f05d1541d57956f4b4ec6722872
harsh9850 <harshkadam3393@gmail.com>
2022-05-11 21:39:22 +0530

    done1

---
+++ hello.java
@@ -0,0 +1,10 @@
+ class hello
+{ public static void main(String[] args) {
+	      
+		
+		System.out.println("hi my name is harsh"); 
+	        System.out.println("hi my name is Machine"); 
+	
+	 
+}
+}
